d2aae51e9d7a9767064c117cbab82564c0b59584
RichardBray01 <rjbray1@gmail.com>
2015-09-12 12:16:44 +0300

    chart and SignalR working together

---
+++ Squawk/Hubs/MessagingHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Squawk
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+            SendChartData();
+        }
+
+        void SendChartData()
+        {
+            Clients.All.asyncMessageReceiver();
+
+        }
+    }
+}
--- Squawk/Startup.cs
+++ Squawk/Startup.cs
@@ -8,7 +8,7 @@ namespace Squawk
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
